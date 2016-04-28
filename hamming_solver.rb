522ff4d65c51cd80362b317a26380a4cdf40e217
Allen Garvey <allengarvey@hotmail.com>
2016-04-27 23:22:35 -0400

    refactor to use new binary string and hamming checker classes and to solve and correct all 1 bit errors in binary string of arbitrary length

--- hamming_solver.rb
+++ hamming_solver.rb
@@ -1,131 +1,73 @@
-def isBinary(s)
-	if s =~ /^[01]+$/
-		true
-	else
-		false
-	end
-end
+require './binary_string.rb'
+require './hamming_checker.rb'
 
-def checkHammingCode(binaryString, startPos, cycleLength, parity='even')
-	numOnes = 0
-	cycleNum = 0
-	(startPos..binaryString.length - 1).each do |i|
-		normalizedPosition = i - startPos
-		if normalizedPosition > 0 && normalizedPosition % cycleLength == 0
-			cycleNum += 1
-		end
-		if cycleNum % 2 == 0 and binaryString[i] == '1'
-			numOnes += 1
-		end
-		# puts "cycleNum is #{cycleNum}"
-		# puts "current char is #{binaryString[i]}"
-	end
-	if (numOnes.even? and parity == 'even') or (!numOnes.even? and parity == 'odd')
-		true
-	else
-		false
-	end
-end
+binary_string = BinaryString.new '100011001010'
+LENGTH = 12
 
-def binaryPrettyString(binaryString)
-	prettyString = ''
-	times = (binaryString.length / 4.0).ceil
-	(0..times).each do |i|
-		start = i * 4
-		prettyString += binaryString.slice start, 4
-		prettyString += ' '
-	end
-
-	prettyString
-end
-
-def numErrors(codeStatuses)
-	codeStatuses.inject(0) do |total, status|  
-		if status
-			total + 1
-		else
-			total
-		end
-	end
-end
-
-def errorIndexes(codeStatuses)
-	errors = []
-	(0..codeStatuses.length - 1).each do |i|
-		if !codeStatuses[i]
-			errors.push i
-		end
-	end
-	errors
-end
-
-def flipBit(binaryString, index)
-	char = binaryString[index]
-	if char == '1'
-		flip = '0'
-	else
-		flip = '1'
-	end
-	binaryString[index] = flip
-	binaryString
-end
-
-
-binary_string = '100011001010'
 
 #validate binary string
-if !isBinary(binary_string)
+if !binary_string.valid?
 	abort "#{binary_string} contains digits other than 0 and 1"
-elsif binary_string.length != 12
-	abort "#{binary_string} is not 12 digits"
+elsif binary_string.source.length != LENGTH
+	abort "#{binary_string} is not #{LENGTH} digits"
 end
 
 
 
-
-codePowers = (0..3)
-codeStatuses = []
-
+hammingCheckers = []
 #calculate hamming codes
-codePowers.each do |power|
-	status = checkHammingCode(binary_string, 2**power - 1, 2**power)
-	codeStatuses.push status
+(0..Math.log(LENGTH, 2).floor).each do |i|
+	hammingCheckers.push HammingChecker.new(i)
 end
 
 #print intro
 puts "Hamming Solver"
-puts "This solver will correct a 12 bit binary Hamming encoded string"
+puts "This solver will correct a #{LENGTH} bit binary Hamming encoded string"
 puts "of an error of 1 bit\n\n"
 #print results
 puts 'Hamming codes results for:'
-puts binaryPrettyString(binary_string)
+puts binary_string.pretty
 puts "\n"
-codePowers.each do |i|
-	if i == 0
-		power = 0
-	else
-		power = 2 ** i
-	end
-	puts "#{power} Code is #{codeStatuses[i]}"
+hammingCheckers.each do |checker|
+	puts "#{checker.index} Code is #{checker.valid?(binary_string)}"
 end
 
 
-errorIndexesArray = errorIndexes(codeStatuses)
-indexToFlip = nil
+error_indexes_array = nil
+valid_indexes_array = []
+
+hammingCheckers.each do |checker|
+	indexes = checker.indexes(binary_string.length)
+	if !checker.valid? binary_string
+		if error_indexes_array.nil?
+			error_indexes_array = indexes
+		else
+			# intersection
+			error_indexes_array = error_indexes_array & indexes
+		end
+	else
+		# union
+		valid_indexes_array = valid_indexes_array | indexes
+	end
+end
 
 puts ""
-case errorIndexesArray.length
-	when 0
-		puts 'Hooray! No errors found'
-	when 1
-		indexToFlip = 2 ** errorIndexesArray[0] - 1
-	else
-		puts 'Not handled yet'
+
+if error_indexes_array.nil?
+	puts "Hooray! No errors found"
+	exit()
+else
+	#difference between errors and valid indexes show indexes that need to be corrected
+	correction_indexes = error_indexes_array - valid_indexes_array
 end
 
-unless indexToFlip.nil?
-	puts 'Corrected string should be:'
-	puts binaryPrettyString(flipBit(binary_string, indexToFlip))
+if correction_indexes.length > 1
+	puts "Too many errors found (#{correction_indexes.length})"
+else
+	puts "Error found at index #{correction_indexes[0]}"
+	puts "Original:\t#{binary_string.pretty}"
+	puts "Corrected:\t#{binary_string.flipBitAt(correction_indexes[0]).pretty}"
 end
+
 
 
